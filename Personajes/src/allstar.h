68f25dee4db820dcd0970bb1a8e6c8616617f3ae
NachoManzano17 <n.manzano2004@gmail.com>
2026-04-23 12:53:10 +0200

    Delete Personajes/src directory

--- Personajes/src/allstar.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "terrestre.h"
-
-class allstar : public terrestre {
-public:
-    allstar(int bando);
-    void atacarEnArena() override;
-};
--- Personajes/src/pomelo.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "terrestre.h"
-
-class pomelo : public terrestre {
-public:
-    pomelo(int bando);
-    void atacarEnArena() override;
-};
