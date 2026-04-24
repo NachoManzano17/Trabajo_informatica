069a90ff579787feae40db403f27307aca94bded
NachoManzano17 <n.manzano2004@gmail.com>
2026-04-25 01:15:55 +0200

    Format volador.h and remove unnecessary comments

--- JUEGO/src/volador.h
+++ JUEGO/src/volador.h
@@ -10,10 +10,7 @@ public:
     // Aquí sí decimos cómo se mueve en el tablero sobrescribiendo (override) el método del padre
     void moverEnTablero() override;
 
-    // OJO: Todavía no definimos atacarEnArena(). Esta clase sigue siendo abstracta, 
-    // lo cual es correcto porque "Terrestre" es un concepto, no un personaje final.
-
-    // --- CUMPLE EL CONTRATO DEL ÁRBITRO (Temporal) ---
+   
     bool esmovimientovalido(int fori, int cori, int fdest, int cdest) override; 
     bool puedeSaltar() const override { return true; }
     
