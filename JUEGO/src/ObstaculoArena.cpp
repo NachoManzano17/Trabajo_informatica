56b29b20a4c1da356395e65e83db18718dcbd26d
izone-rgb <ivr2072@gmail.com>
2026-05-30 20:13:47 +0200

    estética ya va aparte

--- JUEGO/src/ObstaculoArena.cpp
+++ JUEGO/src/ObstaculoArena.cpp
@@ -1,17 +1,10 @@
-// ObstaculoArena.cpp
 #include "ObstaculoArena.h"
 
+// Aquí le damos el cuerpo al constructor 
 ObstaculoArena::ObstaculoArena(float startX, float startY, float w, float h) {
     posicion.x = startX;
     posicion.y = startY;
     ancho = w;
     alto = h;
-
-    shape.setPosition(posicion.x, posicion.y);
-    shape.setSize(sf::Vector2f(w, h));
-    shape.setFillColor(sf::Color(100, 100, 100)); // Gris
 }
 
-void ObstaculoArena::dibujar(sf::RenderWindow& window) {
-    window.draw(shape);
-}
